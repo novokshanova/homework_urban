fe91c51adac3402fcd17d7eb58823399827855ca
Karine <karnov1002@mail.ru>
2025-01-10 23:24:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+s = "1"
+b = 1
+b = str(b)
+print(s + b)
---
+++ module_1_3.py
@@ -0,0 +1,5 @@
+example = "simultaneously"
+print(example[0], example[-1])
+print(example[7:14])
+print(example[-1::-1])
+print(example[0:14:2])
