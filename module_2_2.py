79e3178c786e9427582b41f6d33566788d59a228
Karine <karnov1002@mail.ru>
2025-01-11 04:03:52 +0300

    Initial commit

---
+++ module_2_2.py
@@ -0,0 +1,8 @@
+first = int(input("первое число: "))
+second = int(input("второе число: "))
+third = int(input("третье число: "))
+if first == second == third:
+    print (3)
+elif first == second or second == third or first == second:
+    print (2)
+else: print(0)
