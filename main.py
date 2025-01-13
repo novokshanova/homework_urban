299480a0a158deb4eb3f9ac09c40ace1d31e2388
Karine <karnov1002@mail.ru>
2025-01-13 23:02:07 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-s = "1"
-b = 1
-b = str(b)
-print(s + b)
+# s = "1"
+# b = 1
+# b = str(b)
+# print(s + b)
+
+def file_operations(filename, text_to_add):
+
+    with open(filename, 'a') as file:
+
+        file.write(text_to_add + '\n')
+
+
+    with open(filename, 'r') as file:
+
+        content = file.read()
+        print("Содержимое файла:\n", content)
+
+
+    with open(filename, 'r') as file:
+        print("Построчное чтение файла:")
+        for line in file:
+            print(line.strip())
